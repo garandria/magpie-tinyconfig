3d1d64e403deda69b76fdad5c2347d1fae7082f4
RANDRIANAINA Georges Aaron <randrianaina.georgesaaron@gmail.com>
2025-07-22 19:59:48 +0200

    fix grep invocation that generated false default value

--- scgen.sh
+++ scgen.sh
@@ -9,7 +9,7 @@ cat <(grep bool ${tmp}) <(grep tristate ${tmp}) | cut -d, -f1 > ${tmp}.2
 mv ${tmp}.2 ${tmp}
 cat ${tmp} | while read option ; do
     printf "${option} {True, False}"
-    if [[ $(grep -q "CONFIG_${option}=" linux-6.6-x86.defconfig) ]] ; then
+    if grep -q "CONFIG_${option}=" linux-6.6-x86.defconfig  ; then
 	echo "[True]"
     else
 	echo "[False]"
