7d58f78a04892bf31a83694e034208d2ae205785
RANDRIANAINA Georges Aaron <randrianaina.georgesaaron@gmail.com>
2025-07-23 17:25:24 +0200

    feed directly the script's input to klocalizer since the prefix are already set

--- compile.sh
+++ compile.sh
@@ -2,23 +2,16 @@
 
 cd linux-6.6
 
-tmp=.tmp.args
-
-for arg in ${@} ; do
-    echo "--define ${arg}" >> ${tmp}
-done
-
 klocalizer			\
     --arch x86_64		\
     --match .config		\
     --show-unsat-core		\
-    $(paste -s -d ' ' ${tmp})
+    ${@}
 
 if [[ $? -ne 0 ]]; then
     exit 1
 fi
 
-rm ${tmp}
 make olddefconfig
 make -j$(nproc)
 
