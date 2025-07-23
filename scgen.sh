3f034b02f051cf5cf536f704c7a2d1460c020018
RANDRIANAINA Georges Aaron <randrianaina.georgesaaron@gmail.com>
2025-07-23 17:24:15 +0200

    fix magpie's constants

--- scgen.sh
+++ scgen.sh
@@ -8,9 +8,12 @@ grep ${subsys} linux-6.6-x86-options | cut -d, -f1-2 > ${tmp}
 cat <(grep bool ${tmp}) <(grep tristate ${tmp}) | cut -d, -f1 > ${tmp}.2
 mv ${tmp}.2 ${tmp}
 cat <<EOF
-CLI_PREFIX="CONFIG_"
+TIMING="compile"
+CLI_PREFIX=""
 CLI_GLUE="="
-CLI_BOOLEAN="hide"
+CLI_BOOLEAN_PREFIX_TRUE="--define CONFIG_"
+CLI_BOOLEAN_PREFIX_FALSE="--undefine CONFIG_"
+CLI_BOOLEAN="prefix"
 EOF
 cat ${tmp} | while read option ; do
     printf "${option} {True, False}"
